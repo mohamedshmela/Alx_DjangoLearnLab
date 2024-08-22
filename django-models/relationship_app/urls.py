47151d2e9d8fcfa7b3fc8adcab5411749a858d96
mohamedshmela <mohamedshmela339@gmail.com>
2024-08-22 16:11:31 +0300

    d

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
 from .views import LibraryDetailView
 from .views import list_books
-from . import views
 from .views import (user_login, user_logout, user_register)
-from .views import (
-    BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView,
-    add_book, edit_book, delete_book,
-    user_login, user_logout, user_register,
-    admin_view, librarian_view, member_view,
-    LibraryDetailView,
-)
 
 urlpatterns = [
     path('list_books/',list_books, name= 'list_books'),
