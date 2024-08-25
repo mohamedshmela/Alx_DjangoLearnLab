991f15db1254d401ae4a6a1c1627996319bf8b7e
mohamedshmela <mohamedshmela339@gmail.com>
2024-08-25 18:25:39 +0300

    d

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+from django.contrib.auth.decorators import permission_required
+from relationship_app.forms import BookForm
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+@permission_required('bookshelf.can_edit', raise_exception= True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/edit_book.html', {'form': form})
