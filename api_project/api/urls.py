d23fdf790bb09d906639f6483a9129bb08e006b2
mohamedshmela <mohamedshmela339@gmail.com>
2024-08-29 01:28:13 +0300

    third task

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books', BookList)
 
 urlpatterns = [
-    path('books/', BookList.as_view(),name= 'book_list'),
+    # path('books/', BookList.as_view(),name= 'book_list'),
+    path('',include(router.urls))
 ]
