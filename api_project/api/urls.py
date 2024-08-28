8f9574dbaf70cafbacf5e62d1ebfaa0f69e66448
mohamedshmela <mohamedshmela339@gmail.com>
2024-08-29 02:11:40 +0300

    b

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,6 +8,6 @@ router.register(r'books', BookViewSet)
 
 urlpatterns = [
     # path('books/', BookList.as_view(),name= 'book_list'),
-    path('',include(router.urls)),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('',include(router.urls)),    
 ]
