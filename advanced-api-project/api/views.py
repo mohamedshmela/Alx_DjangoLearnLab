74e87e7ad12740e70a234ddd2174da016db60535
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-08 08:47:59 +0300

    a

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
@@ -8,4 +7,5 @@ from .serializers import AuthorSerializer, BookSerializer
 class ListView(generics.ListApiView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
 
