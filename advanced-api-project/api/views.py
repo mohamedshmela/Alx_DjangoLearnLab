0ef6a0c9209195a4f36c03db9dd4b00ad32d1f5c
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-08 06:45:17 +0300

    a

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+
+
