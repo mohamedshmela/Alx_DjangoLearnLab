572b5fc7ea27c6a0804b4142967aa55fff6a5f13
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-08 06:27:09 +0300

    b

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# the Author model describes how the Author should be
 class Author(models.Model):
     name = models.CharField(max_length= 20)
 
-
+# the Book model describes how the Book should be 
 class Book(models.Model):
     title = models.CharField(max_length= 20)
     publication_year = models.IntegerField()
