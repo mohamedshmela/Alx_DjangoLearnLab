1402dac6c08c84b800c86e5c760d60204b8e6712
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-08 06:48:08 +0300

    a

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,4 +4,6 @@ from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
 
-
+class CustomBookListView(generics.ListApiView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
