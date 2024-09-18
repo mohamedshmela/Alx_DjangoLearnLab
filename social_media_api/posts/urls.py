70b7a7e2dd7ba05552d93e3d41acfd5b7b62ac01
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-18 06:33:01 +0300

    e

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -4,6 +4,6 @@ from .views import PostViewSet, CommentViewSet, UserFeedView
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'feed', UserFeedView)
+router.register(r'feed/', UserFeedView)
 
 urlpatterns = router.urls
