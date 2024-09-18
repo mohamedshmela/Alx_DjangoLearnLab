7771361c2d494be931dc84d3fc37afaff2c3dcdf
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-18 04:47:30 +0300

    changes to task1

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,29 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import CustomUser
+from rest_framework.authtoken.models import Token
 
-class UserSerializer(serializers.ModelSerializer):
+CustomUser = get_user_model()  # Get the custom user model
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # Ensure password is write-only
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+
+    def create(self, validated_data):
+        # Create a new user
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        # Add optional fields like bio and profile picture
+        user.bio = validated_data.get('bio', '')
+        user.profile_picture = validated_data.get('profile_picture', None)
+        user.save()
+
+        # Create a token for the new user
+        Token.objects.create(user=user)
+
+        return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -4,15 +4,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status, generics
 from .models import CustomUser
-from .serializers import UserSerializer
+from .serializers import UserRegistrationSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
     def post(self, request):
-        serializer = UserSerializer(data= request.data)
+        serializer = UserRegistrationSerializer(data= request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
+            token = Token.objects.get(user=user) # Get the token associated with the new user
             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
