40bb21e0e7cd42b7634d42ce6e87aef8bf9bbc0d
mohamedshmela <mohamedshmela339@gmail.com>
2024-09-18 05:21:57 +0300

    second task

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'accounts',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'posts',
 ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
@@ -48,7 +49,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
+
+REST_FRAMEWORK['DEFAULT_FILTER_BACKENDS'] = ['django_filters.rest_framework.DjangoFilterBackend']
+
 
 
 MIDDLEWARE = [
